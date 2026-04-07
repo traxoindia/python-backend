20671bc8e992f7d6c89dc283faced9873d691cf8
sonali-barik <bariksonali178@gmail.com>
2026-04-07 12:16:16 +0530

    python backend

--- app/main.py
+++ app/main.py
@@ -11,3 +11,25 @@ def home():
 from app.routes.company_routes import router as company_router
 
 app.include_router(company_router)
+from fastapi import FastAPI
+from app.routes.auth_routes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 🔥 allow all (for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+@app.get("/")
+def home():
+    return {"message": "Admin Backend Running"}
+
+from app.routes.company_routes import router as company_router
+
+app.include_router(company_router)
