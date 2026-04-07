59474a9143d212704dfe6c0ffe8bb6cf4ec468d5
sonali-barik <bariksonali178@gmail.com>
2026-04-07 10:34:19 +0530

    python backend

--- app/utils/hash.py
+++ app/utils/hash.py
@@ -21,13 +21,13 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password):
-    # ✅ Prevent bcrypt crash
-    password = password[:72]
+def hash_password(password: str):
+    if len(password.encode("utf-8")) > 72:
+        raise ValueError("Password too long (max 72 bytes)")
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str):
     if len(plain_password.encode("utf-8")) > 72:
         return False
-    
     return pwd_context.verify(plain_password, hashed_password)
